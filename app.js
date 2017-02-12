66e9098c6983c890bfc6360bf55dfcba4b18ffed
Patrick Wilver <pwilver12@gmail.com>
2017-02-12 18:56:02 -0500

    First test cases to requests at the api root

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import cors from 'cors';
 import express from 'express';
-// import path from 'path';
 
 import apiRoutes from './routes/api';
 import { API_VERSION } from './src/js/variables/constants';
@@ -9,17 +8,11 @@ import { API_VERSION } from './src/js/variables/constants';
 const app = express();
 
 // APP CONFIG
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-// app.use(express.static(path.join(__dirname, './build')));
 app.use(cors());
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'OK',
-    version: 1
-  });
+  res.status(200).send('Hello World!');
 });
 
 app.use(`/api/${API_VERSION}`, apiRoutes);
