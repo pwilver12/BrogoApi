1ecf8a519f4997c58dd89801fccc27e0187444b7
Patrick Wilver <pwilver@hubspot.com>
2017-02-12 23:17:36 -0500

    Add routers for all api paths
    Add comments indicating what all routes should eventually accomplish

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,41 @@ const express = require('express');
 
 const router = express.Router();
 
-// POST NEW GAME
+// Retrieve all games/ add new game
 router.route('/games')
   .get((req, res) => {
-    //
+    // Filterable by team ID
+    // Return games as json
   })
   .post((req, res) => {
-    //
+    // 201, return json for new game
+    // Add new game to page listing immediately
   });
 
+// Retrieve or delete single game
+router.route('/games/:game_id')
+  .get((req, res) => {
+    // Return single game as json
+  })
+  .delete((req, res) => {
+    // ERROR: when no id, throw 400 Bad request
+    // SUCCESS: 204 No content
+  });
+
+// Retrieve all teams/ add new team
+router.route('/teams')
+  .get((req, res) => {
+    // Return teams as json
+  })
+  .post((req, res) => {
+    // 201, return json for new team (just name?)
+    // Add new team to page listing immediately
+  });
+
+// Delete a team
+router.route('/teams/:team_id')
+  .delete((req, res) => {
+    // ERROR: when no id, throw 400 Bad request
+    // SUCCESS: 204 No content
+  });
 export { router as default };
