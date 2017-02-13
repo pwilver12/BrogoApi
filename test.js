a16eb96b5ef03e6408b756ceddfebd2965f94a38
Patrick Wilver <pwilver12@gmail.com>
2017-02-13 00:18:30 -0500

    Install body-parser to properly parse post request data
    Update POST `/games` test cases to validate request data and check for presence of response data

--- test.js
+++ test.js
@@ -35,9 +35,35 @@ describe('Retrieving game information', () => {
 
 // POST /api/v1/games
 describe('Creating a new game', () => {
+  // Check for correct status code
   it('Returns 201 status code', done => {
     request(app)
       .post(`${API_PATH}/games`)
+      .send('team_1=patim&team_2=scoben&type=ledger&diff=8')
       .expect(201, done);
+  });
+
+  // Check for proper JSON format
+  it('Returns JSON format', done => {
+    request(app)
+      .post(`${API_PATH}/games`)
+      .send('team_1=patim&team_2=scoben&type=ledger&diff=8')
+      .expect('Content-Type', /json/, done);
+  });
+
+  // Check for proper JSON format
+  it('Returns new game data', done => {
+    request(app)
+      .post(`${API_PATH}/games`)
+      .send('team_1=patim&team_2=scoben&type=ledger&diff=8')
+      .expect(/ledger/i, done);
+  });
+
+  // Validate request
+  it('Validates required fields', done => {
+    request(app)
+      .post(`${API_PATH}/games`)
+      .send('team_1=&team_2=&type=&diff=')
+      .expect(400, done);
   });
 });
